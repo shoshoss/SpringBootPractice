e1c40532ad25a026804239ab15ef40a8456eb55b
shota <s8152.45@gmail.com>
2025-06-16 14:00:06 +0900

    ContactService に一覧取得処理を追加

--- practice/src/main/java/com/example/demo/contact/service/ContactServiceImpl.java
+++ practice/src/main/java/com/example/demo/contact/service/ContactServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.contact.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +29,10 @@ public class ContactServiceImpl implements ContactService {
 		contact.setBody(contactForm.getBody());
 		
 		contactRepository.save(contact);
+	}
+	
+	@Override
+	public List<Contact> getAllContacts() {
+		return contactRepository.findAll();
 	}
 }
