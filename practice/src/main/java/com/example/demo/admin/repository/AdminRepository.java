80aeb57c148ed47b76d02a087adb57337b9eae4a
shota <s8152.45@gmail.com>
2025-06-26 12:59:47 +0900

    MyBatisなど必要のないコードの削除

--- practice/src/main/java/com/example/demo/admin/repository/AdminRepository.java
+++ practice/src/main/java/com/example/demo/admin/repository/AdminRepository.java
@@ -2,16 +2,12 @@ package com.example.demo.admin.repository;
 
 import java.util.Optional;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.admin.entity.Admin;
 
-@Mapper
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 	boolean existsByEmail(String email);
 	
-	@Select("select * from admins where email = #{email}")
     Optional<Admin> findByEmail(String email);
 }
--- practice/src/main/java/com/example/demo/admin/service/AppUserDetailsService.java
+++ practice/src/main/java/com/example/demo/admin/service/AppUserDetailsService.java
@@ -1,11 +1,9 @@
 package com.example.demo.admin.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.admin.entity.Admin;
@@ -16,8 +14,7 @@ public class AppUserDetailsService implements UserDetailsService {
 
 		private final AdminRepository adminRepository;
 		
-		@Autowired
-		public AppUserDetailsService(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
+		public AppUserDetailsService(AdminRepository adminRepository) {
 			this.adminRepository = adminRepository;
 		}
 		
